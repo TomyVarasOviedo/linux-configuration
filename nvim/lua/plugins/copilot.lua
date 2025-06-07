2851542bb3d9a2935aeaef6071a75faea1ae5fdf
TomyVarasOviedo <tomasvarasoviedo@gmail.com>
2025-06-07 16:05:46 -0300

    fix:configuraciones y modificaciones

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,8 +1,1 @@
-return {
-  "Exafunction/codeium.vim",
-  event = "InsertEnter",
-  config = function()
-    -- Configuración básica
-    vim.g.codeium_no_map_tab = 1 -- Desactiva el mapeo de <Tab> por defecto
-  end,
-}
+return { "github/copilot.vim" }
