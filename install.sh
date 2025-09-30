c5bffa013d41987af56e616c5ccadfb571189325
Tomás Varas Oviedo <88008465+TomyVarasOviedo@users.noreply.github.com>
2025-09-30 15:37:01 -0300

    fix: Instalacion de fuentes

--- install.sh
+++ install.sh
@@ -1,4 +1,14 @@
 !# /bin/bash
 
 #Instalacion automatica
-sudo apt install neovim rofi i3 i3status ranger picom kitty libreoffice vscode vim lsd
+sudo apt install ./nvim-linux-x86_64.deb rofi i3 i3status ranger picom kitty libreoffice vim lsd
+
+# Instalar fuentes
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/FiraCode.zip
+
+unzip FiraCode.zip
+
+sudo mv -t /usr/share/fonts/ $(ls | grep FiraCode)
+
+#Limpiar la cache de las fuentes
+fc-cache -t -v
